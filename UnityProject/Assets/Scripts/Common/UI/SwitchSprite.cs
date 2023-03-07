8e0725c25f74bd9bd5f17ebe786e48440ded52af
TAGOMORI\tago1 <tago1113@gmail.com>
2023-03-07 22:12:48 +0900

    更新（追加）

---
+++ UnityProject/Assets/Scripts/Common/UI/SwitchSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonUI
+{
+	/// <summary>
+	/// 表示切替
+	/// </summary>
+	public class SwitchSprite : MonoBehaviour
+	{
+		[SerializeField]
+		private UnityEngine.UI.Image m_targetImage;
+
+		[SerializeField]
+		private Sprite m_on;
+
+		[SerializeField]
+		private Sprite m_off;
+
+		public void Setup(bool value)
+		{
+			m_targetImage.sprite = (value == true) ? m_on : m_off;
+		}
+	}
+}
